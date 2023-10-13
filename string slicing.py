66857243ff3870163bfe628c420a204fa50ca5b2
AhmedPASH <143686870+AhmedPASH@users.noreply.github.com>
2023-10-13 15:55:49 +0530

    Add files via upload

---
+++ string slicing.py
@@ -0,0 +1,42 @@
+#string slicing
+
+name="ahmed"
+print(name[0:3])
+
+print(name[0:])
+
+print(name[0:5])
+
+print(name[:])
+
+print(name[0:2])
+
+print(name[0:1])
+
+# reverse a string
+
+print(name[-6:-1])
+
+print(name[-6:])
+
+print(name[:-1])
+
+print(name[:-5])
+
+#with out slicing
+
+n = "ahmed"
+new_string = " "
+for i in n:
+    new_string = i + new_string
+print(new_string)
+
+
+#reverse list
+attendees:list = ["one", "two","three"]
+
+print(attendees[::-2])
+
+
+
+
